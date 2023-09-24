2bade9216a7289850876d03ec80c7f608434531b
hSangg <sangfc774@gmail.com>
2023-09-24 14:01:38 +0700

    update

--- app/layout.js
+++ app/layout.js
@@ -12,16 +12,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <title>WEB</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;800;900&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-
       <body className={inter.className}>{children}</body>
     </html>
   );
